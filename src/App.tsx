8aff3617b0e30b07ecc50dcacc5b33691ef789b9
Austin Theriot <austinmtheriot@gmail.com>
2020-12-11 18:12:53 -0600

    preliminary object-oriented animation test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './App.module.scss';
+import { Animation } from 'animation/test';
 
 function App() {
+	const canvas = useRef<HTMLCanvasElement | null>(null);
+
+	useEffect(() => {
+		if (canvas.current == null) return;
+		const animation = new Animation(canvas.current);
+		animation.animate();
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [canvas]);
+
 	return (
 		<div className={styles.App}>
-			<button>example</button>
+			<canvas ref={canvas}></canvas>
 		</div>
 	);
 }
---
+++ src/animation/test.ts
@@ -0,0 +1,77 @@
+class Particle {
+	x: number;
+	y: number;
+	radius: number;
+	color: string;
+	ctx: CanvasRenderingContext2D;
+	speed: number;
+
+	constructor(ctx: CanvasRenderingContext2D) {
+		this.x = Math.random() * 499;
+		this.y = Math.random() * 499;
+		this.radius = 1;
+		this.color = '#000000';
+		this.ctx = ctx;
+		this.speed = Math.random() * 5;
+	}
+
+	private _clampValue(min: number, value: number, max: number): number {
+		return Math.max(Math.min(max, value), min);
+	}
+
+	private _randomWalk(currentPosition: number) {
+		return this._clampValue(
+			0,
+			currentPosition + (Math.random() * this.speed - this.speed / 2),
+			500
+		);
+	}
+
+	public draw() {
+		this.ctx.beginPath();
+		this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		this.ctx.closePath();
+		this.ctx.fillStyle = this.color;
+		this.ctx.fill();
+	}
+
+	public move() {
+		this.x = this._randomWalk(this.x);
+		this.y = this._randomWalk(this.y);
+	}
+}
+
+export class Animation {
+	canvas: HTMLCanvasElement;
+	ctx: CanvasRenderingContext2D;
+	particles: Particle[];
+
+	constructor(canvas: HTMLCanvasElement) {
+		this.canvas = canvas;
+		this.canvas.width = 500;
+		this.canvas.height = 500;
+		this.ctx = canvas.getContext('2d')!;
+		this.particles = [];
+	}
+
+	private _init() {
+		//Init scene
+		this.particles = new Array(1000)
+			.fill(null)
+			.map(() => new Particle(this.ctx));
+	}
+
+	private _draw() {
+		this.ctx.clearRect(0, 0, 500, 500); // clear canvas
+		this.particles.forEach((particle) => {
+			particle.move();
+			particle.draw();
+		});
+		window.requestAnimationFrame(() => this._draw());
+	}
+
+	public animate() {
+		this._init();
+		window.requestAnimationFrame(() => this._draw());
+	}
+}
