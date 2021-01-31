270b9ff763321472061268542fcfc6c5839b9db5
Austin Theriot <austinmtheriot@gmail.com>
2021-01-31 13:25:43 -0600

    increase canvas size

--- src/classes/Animation.ts
+++ src/classes/Animation.ts
@@ -4,8 +4,8 @@ export class Animation {
 
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
-		this.canvas.width = 500;
-		this.canvas.height = 500;
+		this.canvas.width = 1000;
+		this.canvas.height = 1000;
 		if (!canvas.getContext('2d')) alert('2D canvas not supported!');
 		this.ctx = canvas.getContext('2d')!;
 	}
